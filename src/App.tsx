8fe62d63e97c3eef4e9a93141ea60faaceefddf9
Abdusselam Kelani <abdusselam.kelani@gmail.com>
2023-10-19 17:50:45 +0300

    styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import ToDoList from './components/ToDoList';
 function App() {
   return (
-    <div className="mx-5 h-full w-full md:mx-0">
+    <div className="h-full w-full md:mx-0">
       <ToDoList />
     </div>
   );
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Input, Button, Box, Title } from '@mantine/core';
+import { Input, Button, Box } from '@mantine/core';
 import Task from './Task';
 
 const ToDoList = () => {
@@ -37,44 +37,46 @@ const ToDoList = () => {
   };
 
   return (
-    <Box maw={750} mx="auto" className="pt-6 md:pt-20">
-      <div className="mb-5">
-        <Title order={1}>ToDo List App</Title>
-      </div>
-      <div className="w-full items-center justify-between gap-3 gap-x-3 md:flex">
-        <Input
-          variant="filled"
-          style={{ flex: 1 }}
-          size={window.innerWidth <= 768 ? 'md' : 'lg'}
-          placeholder="Add new task"
-          radius="sm"
-          value={task}
-          onChange={(e) => setTask(e.target.value)}
-        />
-        <div>
-          <Button
-            className="mt-3 w-[100%] md:mt-0 md:w-28"
-            fullWidth
-            variant="filled"
-            size={window.innerWidth <= 768 ? 'sm' : 'lg'}
-            radius="sm"
-            onClick={AddNewTask}
-          >
-            Add
-          </Button>
+    <Box maw={750} mx="auto" className="md:mt-20 md:rounded-xl md:bg-[#1d3557] px-10 py-8 md:shadow-xl">
+      <div>
+        <div className="mb-5 flex justify-center text-white">
+          <h1>ToDo List App</h1>
         </div>
-      </div>
-
-      {/* Tasks */}
-      <div className="mt-5">
-        {tasks.map((taskText, index) => (
-          <Task
-            key={index}
-            taskText={taskText}
-            onDelete={() => deleteTask(index)}
-            onEdit={(newText) => editTask(index, newText)}
+        <div className="w-full items-center justify-between gap-3 gap-x-3 md:flex">
+          <Input
+            variant="filled"
+            style={{ flex: 1 }}
+            size={window.innerWidth <= 768 ? 'md' : 'lg'}
+            placeholder="Add new task"
+            radius="sm"
+            value={task}
+            onChange={(e) => setTask(e.target.value)}
           />
-        ))}
+          <div>
+            <Button
+              className="mt-3 w-[100%] md:mt-0 md:w-28"
+              fullWidth
+              variant="filled"
+              size={window.innerWidth <= 768 ? 'sm' : 'lg'}
+              radius="sm"
+              onClick={AddNewTask}
+            >
+              Add
+            </Button>
+          </div>
+        </div>
+
+        {/* Tasks */}
+        <div className="mt-5">
+          {tasks.map((taskText, index) => (
+            <Task
+              key={index}
+              taskText={taskText}
+              onDelete={() => deleteTask(index)}
+              onEdit={(newText) => editTask(index, newText)}
+            />
+          ))}
+        </div>
       </div>
     </Box>
   );
